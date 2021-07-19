d379029bbb5f7ff0bce6fe7c01d839dd15af2fa3
jlswbs <jlswbs@users.noreply.github.com>
2021-07-19 21:35:51 +0200

    Delete Turmites directory

--- Turmites/c.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem Compilation...
-
-if exist st7789_lcd.pio.h del st7789_lcd.pio.h
-..\_exe\pioasm.exe -o c-sdk src\st7789_lcd.pio st7789_lcd.pio.h
-
-call ..\_c1.bat
--- Turmites/d.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem Delete...
-
-if exist st7789_lcd.pio.h del st7789_lcd.pio.h
-call ..\_d1.bat
